6e36ba826bccde277f345adf0c0723dc6e65e05e
Jaime Andres  Lombana Gonzalez <alombana24@hotmail.com>
2023-09-27 08:14:14 -0500

    diseno pantalla recepcion data bluethooth

--- Flutter/flutter_bluethooth/lib/blueThooth/ChatPage.dart
+++ Flutter/flutter_bluethooth/lib/blueThooth/ChatPage.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:keep_screen_on/keep_screen_on.dart';
 
 class ChatPage extends StatefulWidget {
   final BluetoothDevice server;
@@ -27,10 +28,10 @@ class _ChatPage extends State<ChatPage> {
 
   List<_Message> messages = [];
   String _messageBuffer = '';
+  String dataFuerza = '0';
 
-  final TextEditingController textEditingController =
-      new TextEditingController();
-  final ScrollController listScrollController = new ScrollController();
+  final TextEditingController textEditingController = TextEditingController();
+  final ScrollController listScrollController = ScrollController();
 
   bool isConnecting = true;
   bool isDisconnecting = false;
@@ -38,7 +39,7 @@ class _ChatPage extends State<ChatPage> {
   @override
   void initState() {
     super.initState();
-
+    KeepScreenOn.turnOn();
     BluetoothConnection.toAddress(widget.server.address).then((_connection) {
       print('Connected to the device');
       connection = _connection;
@@ -83,77 +84,23 @@ class _ChatPage extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    final List<Row> list = messages.map((_message) {
-      return Row(
-        children: <Widget>[
-          Container(
-            child: Text(
-                (text) {
-                  return text == '/shrug' ? '¯\\_(ツ)_/¯' : text;
-                }(_message.text.trim()),
-                style: TextStyle(color: Colors.white)),
-            padding: EdgeInsets.all(12.0),
-            margin: EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
-            width: 222.0,
-            decoration: BoxDecoration(
-                color:
-                    _message.whom == clientID ? Colors.blueAccent : Colors.grey,
-                borderRadius: BorderRadius.circular(7.0)),
-          ),
-        ],
-        mainAxisAlignment: _message.whom == clientID
-            ? MainAxisAlignment.end
-            : MainAxisAlignment.start,
-      );
-    }).toList();
-
     return Scaffold(
-      appBar: AppBar(
-          title: (isConnecting
-              ? Text('Connecting chat to ${widget.server.name}...')
-              : isConnected()
-                  ? Text('Live chat with ${widget.server.name}')
-                  : Text('Chat log with ${widget.server.name}'))),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            Flexible(
-              child: ListView(
-                  padding: const EdgeInsets.all(12.0),
-                  controller: listScrollController,
-                  children: list),
-            ),
-            Row(
-              children: <Widget>[
-                Flexible(
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 16.0),
-                    child: TextField(
-                      style: const TextStyle(fontSize: 15.0),
-                      controller: textEditingController,
-                      decoration: InputDecoration.collapsed(
-                        hintText: isConnecting
-                            ? 'Wait until connected...'
-                            : isConnected()
-                                ? 'Type your message...'
-                                : 'Chat got disconnected',
-                        hintStyle: const TextStyle(color: Colors.grey),
-                      ),
-                      enabled: isConnected(),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(8.0),
-                  child: IconButton(
-                      icon: const Icon(Icons.send),
-                      onPressed: isConnected()
-                          ? () => _sendMessage(textEditingController.text)
-                          : null),
-                ),
-              ],
-            )
-          ],
+      // appBar: AppBar(
+      //     title: (isConnecting
+      //         ? Text('Connecting chat to ${widget.server.name}...')
+      //         : isConnected()
+      //             ? Text('Live chat with ${widget.server.name}')
+      //             : Text('Chat log with ${widget.server.name}'))),
+      body: Container(
+        color: Colors.black,
+        child: Center(
+          child: Text(
+            "$dataFuerza",
+            style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.5,
+                //fontSize: 390,
+                color: Colors.red),
+          ),
         ),
       ),
     );
@@ -208,6 +155,9 @@ class _ChatPage extends State<ChatPage> {
 
       final parsedJson = jsonDecode(dataString);
       print("---LO que llega: ${parsedJson['message']}");
+      setState(() {
+        dataFuerza = parsedJson['message'];
+      });
     }
   }
 
--- Flutter/flutter_bluethooth/lib/main.dart
+++ Flutter/flutter_bluethooth/lib/main.dart
@@ -1,33 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bluethooth/provider/StatusConexaoProvider.dart';
-// import 'package:provider/provider.dart';
-
-// import 'HomePage.dart';
-// import 'SelecionarDispositivoPage.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MultiProvider(
-//         providers: [
-//           ChangeNotifierProvider<StatusConexaoProvider>.value(
-//               value: StatusConexaoProvider()),
-//         ],
-//         child: MaterialApp(
-//           title: 'Xerocasa',
-//           initialRoute: '/',
-//           routes: {
-//             '/': (context) => HomePage(),
-//             '/selectDevice': (context) => const SelecionarDispositivoPage(),
-//           },
-//         ));
-//   }
-// }
 import 'package:flutter/material.dart';
 
 import 'blueThooth/MainPage.dart';
